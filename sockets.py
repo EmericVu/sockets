422e4617313a1113d810fae48e70bfd6b0bc5be4
EmericVu <vuv@lacroixrouge-brest.fr>
2018-12-20 12:14:36 +0100

    socket

--- sockets.py
+++ sockets.py
@@ -7,11 +7,8 @@ MESSAGE= (b"?")
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.sendto("cinema".encode(), (UDP_IP, UDP_PORT))
 
-while True:
-    data, addr = sock.recvfrom(1024)
-    print("received message : ", data)
-    
-    #sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    #print ("send message :")
-    #MESSAGE2= str(input())
-    #sock.sendto(MESSAGE2.encode(), (addr))
+data, addr = sock.recvfrom(1024)
+print("Instruction : ", data)
+code(data)
+data, addr = sock.recvfrom(1024)
+print("Instruction : ", data)
